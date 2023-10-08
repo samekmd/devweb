4efdfb4e27d489b38ee7c862562337af3fa7b52c
samekmd <samuelalkmc@gmail.com>
2023-10-07 23:55:05 -0300

    Cria BASE.HTML
    Criei a base.html, visando reduzir a repetição de códigos em outras páginas, logo o site roda mais limpo.

--- devweb-main/app.py
+++ devweb-main/app.py
@@ -1,13 +1,20 @@
 from flask import Flask, render_template
 
 app = Flask('__name__')
+
 @app.route('/')
 def home():
     return render_template('home.html')
-@app.route('/quemsommos')
+
+@app.route('/Contato')
+def contato():
+    return render_template('contato.html')
+
+@app.route('/Quemsomos')
 def quemsomos():
     return render_template('quemsomos.html')
-@app.route('/contato')
-def contato():
-    return render_template('contato.hmtl')
 
+
+
+
+app.run(debug=True)
